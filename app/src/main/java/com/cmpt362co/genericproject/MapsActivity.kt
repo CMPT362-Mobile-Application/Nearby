e4a71a4a4fa793e9f39595821d24935024f48c8d
IAsianRice <alexcho2001@gmail.com>
2022-07-06 16:27:16 -0700

    Rudimentary UI

--- app/src/main/java/com/cmpt362co/genericproject/MapsActivity.kt
+++ app/src/main/java/com/cmpt362co/genericproject/MapsActivity.kt
@@ -1,17 +1,16 @@
 package com.cmpt362co.genericproject
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.cmpt362co.genericproject.databinding.ActivityMapsBinding
+import com.google.android.gms.maps.*
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback{
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
@@ -21,6 +20,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        supportActionBar?.hide()
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
@@ -39,10 +39,31 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+        mMap.mapType = GoogleMap.MAP_TYPE_NORMAL
 
         // Add a marker in Sydney and move the camera
         val sydney = LatLng(-34.0, 151.0)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
+        mMap.setOnMapClickListener {
+            mMap.addMarker(MarkerOptions().position(it).title("Placed"))
+        }
+
+        mMap.setOnMarkerClickListener {
+            val builder = AlertDialog.Builder(this)
+            builder.setMessage("Marker @: ${it.position.toString()}")
+                .setPositiveButton("More",
+                    DialogInterface.OnClickListener { dialog, id ->
+                        // START THE GAME!
+                    })
+                .setNegativeButton("Cancel",
+                    DialogInterface.OnClickListener { dialog, id ->
+                        // User cancelled the dialog
+                    })
+            // Create the AlertDialog object and return it
+            builder.create().show()
+            return@setOnMarkerClickListener true
+        }
     }
 }
